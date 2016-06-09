69047e56f8c059c7869c9ef38bd64d5cd255a02d
Russell Anderson <rusticlitton@gmail.com>
2016-06-09 18:35:34 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace AndersonLab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbNull = new System.Windows.Forms.RadioButton();
+            this.rbD = new System.Windows.Forms.RadioButton();
+            this.rbC = new System.Windows.Forms.RadioButton();
+            this.rbB = new System.Windows.Forms.RadioButton();
+            this.rbA = new System.Windows.Forms.RadioButton();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Official DMV Drivers Liscense Test";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(91, 201);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(91, 23);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Tag = "";
+            this.btnNext.Text = "Next Question";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbNull);
+            this.groupBox1.Controls.Add(this.rbD);
+            this.groupBox1.Controls.Add(this.rbC);
+            this.groupBox1.Controls.Add(this.rbB);
+            this.groupBox1.Controls.Add(this.rbA);
+            this.groupBox1.Controls.Add(this.lblQuestion);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(51, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(166, 151);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Please choose answer";
+            // 
+            // rbNull
+            // 
+            this.rbNull.AutoSize = true;
+            this.rbNull.Location = new System.Drawing.Point(63, 128);
+            this.rbNull.Name = "rbNull";
+            this.rbNull.Size = new System.Drawing.Size(46, 20);
+            this.rbNull.TabIndex = 5;
+            this.rbNull.TabStop = true;
+            this.rbNull.Text = "null";
+            this.rbNull.UseVisualStyleBackColor = true;
+            this.rbNull.Visible = false;
+            // 
+            // rbD
+            // 
+            this.rbD.AutoSize = true;
+            this.rbD.Location = new System.Drawing.Point(110, 101);
+            this.rbD.Name = "rbD";
+            this.rbD.Size = new System.Drawing.Size(36, 20);
+            this.rbD.TabIndex = 4;
+            this.rbD.TabStop = true;
+            this.rbD.Text = "D";
+            this.rbD.UseVisualStyleBackColor = true;
+            // 
+            // rbC
+            // 
+            this.rbC.AutoSize = true;
+            this.rbC.Location = new System.Drawing.Point(30, 101);
+            this.rbC.Name = "rbC";
+            this.rbC.Size = new System.Drawing.Size(35, 20);
+            this.rbC.TabIndex = 3;
+            this.rbC.TabStop = true;
+            this.rbC.Text = "C";
+            this.rbC.UseVisualStyleBackColor = true;
+            // 
+            // rbB
+            // 
+            this.rbB.AutoSize = true;
+            this.rbB.Location = new System.Drawing.Point(110, 59);
+            this.rbB.Name = "rbB";
+            this.rbB.Size = new System.Drawing.Size(35, 20);
+            this.rbB.TabIndex = 2;
+            this.rbB.TabStop = true;
+            this.rbB.Text = "B";
+            this.rbB.UseVisualStyleBackColor = true;
+            // 
+            // rbA
+            // 
+            this.rbA.AutoSize = true;
+            this.rbA.Location = new System.Drawing.Point(30, 59);
+            this.rbA.Name = "rbA";
+            this.rbA.Size = new System.Drawing.Size(35, 20);
+            this.rbA.TabIndex = 1;
+            this.rbA.TabStop = true;
+            this.rbA.Text = "A";
+            this.rbA.UseVisualStyleBackColor = true;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(48, 27);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(71, 16);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "Question 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(271, 236);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(287, 275);
+            this.MinimumSize = new System.Drawing.Size(287, 275);
+            this.Name = "Form1";
+            this.Text = "DMV Exam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbD;
+        private System.Windows.Forms.RadioButton rbC;
+        private System.Windows.Forms.RadioButton rbB;
+        private System.Windows.Forms.RadioButton rbA;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.RadioButton rbNull;
+        public System.Windows.Forms.Button btnNext;
+    }
+}
+
